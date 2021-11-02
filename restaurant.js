06a33eb2f74f0ca7a07e785b492135f452f6dd7c
Ruth <aylaaeternum@gmail.com>
2021-11-02 16:12:40 +0100

    responsive

--- restaurant.js
+++ restaurant.js
@@ -1,7 +1,17 @@
 var btnMenuOpen = document.getElementById("btnMenuOpen"),
     btnMenuClose = document.getElementById("btnMenuClose"),
-    menuResponsive = document.getElementById("menuBar");
+    menuResponsive = document.getElementById("menu-bar");
+    enlaces = document.getElementById("links");
 
     btnMenuOpen.addEventListener("click", function () {
         menuResponsive.classList.add("active");
+    });
+
+    btnMenuClose.addEventListener("click", function () {
+        menuResponsive.classList.remove("active");
+    });
+
+    enlaces.addEventListener("click", function () {
+        menuResponsive.style.transitionDelay = "0.5s";
+        menuResponsive.classList.remove("active");
     });
