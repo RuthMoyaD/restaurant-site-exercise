fa4a37ecf016b1a6e33dd41172c701847a216ac7
Ruth <aylaaeternum@gmail.com>
2021-11-03 17:14:39 +0100

    scroll

--- restaurant.js
+++ restaurant.js
@@ -63,4 +63,35 @@ var btnMenuOpen = document.getElementById("btnMenuOpen"),
     }
 }
 
+window.addEventListener("scroll", function (){
+    var  scroll = document.documentElement.scrollTop,
+         fullSize = document.documentElement.offsetHeight,
+         sizeVP = document.documentElement.clientHeight;
+
+
+    if (scroll > 100) {
+         btnTop.classList.add("show");
+    }else{
+         btnTop.classList.remove("show");
+    }
+
+    
+    if (fullSize == (scroll + sizeVP)) {
+        btnTop.classList.add("scrollFinal"); 
+    }else{
+         btnTop.classList.remove("scrollFinal");
+    }
+});
+
     form.addEventListener("submit", validate);
+
+    var btnTop = document.getElementById("btn-top");
+    var logo = document.getElementById("logo");
+
+    btnTop.addEventListener("click", function () {
+        window.scrollTo(0,0);
+   });
+
+   logo.addEventListener("click", function () {
+    window.scrollTo(0,0);
+});
